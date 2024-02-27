8e9b19ca7a84c247aab26f60c897505e9cca8285
low048 <146587105+low048@users.noreply.github.com>
2024-02-27 21:01:13 +0200

    aprašyta studentas.h

---
+++ studentas.h
@@ -0,0 +1,19 @@
+#ifndef STUDENTAS_H
+#define STUDENTAS_H
+
+#include <string>
+#include <vector>
+
+struct Studentas {
+    std::string vardas;
+    std::string pavarde;
+    std::vector<int> namuDarbai;
+    int sum = 0; //namų darbų rezultatų suma
+    int egz = 0; //egzamino rezultatas
+    float galutinisVid = 0.0;
+    float galutinisMed = 0.0;
+
+    void apskaiciuotiGalutini();
+};
+
+#endif
