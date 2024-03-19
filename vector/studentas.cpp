803789df6e60f0c67f448ff144156bfd03468dce
Laurynas <146587105+low048@users.noreply.github.com>
2024-03-19 22:28:39 +0200

    pridėtas list versijos aplankas

---
+++ vector/studentas.cpp
@@ -0,0 +1,18 @@
+#include "studentas.h"
+
+void Studentas::apskaiciuotiGalutini() {
+    if(!namuDarbai.empty()){
+        std::sort(namuDarbai.begin(), namuDarbai.end()); //išrikiuoti didėjimo tvarka
+        size_t n = namuDarbai.size();
+        if (n % 2 == 1) {
+            galutinisMed = namuDarbai[n / 2]; //jei namų darbų skaičius yra nelyginis, mediana per vidurį
+        } else {
+            galutinisMed = (namuDarbai[n / 2 - 1] + namuDarbai[n / 2]) / 2.0; //jei ne, mediana dviejų vidurinių skaičių aritmetinis vidurkis
+        }
+        galutinisMed = (galutinisMed * 0.4) + (egz * 0.6);
+        galutinisVid = ((double)sum / n * 0.4) + (egz * 0.6);
+    } else {
+        galutinisMed = (egz * 0.6);
+        galutinisVid = (egz * 0.6);
+    }
+}
